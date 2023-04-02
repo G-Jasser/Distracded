8d85003516f5d3dfb131bf570cdcf8b60df88a49
J-Gharb <J-Gharb>
2023-04-02 23:02:11 +0100

    Updated CommonElements.py Added Mock tests in Test_CommonElements.py

--- Test_CommonElements.py
+++ Test_CommonElements.py
@@ -14,9 +14,16 @@ def test_ce_with_patch_returnValue():
 
 def test_ce_with_patch_sideEffect():
     def ce_patched(l1, l2):
-        return [12, 34, 23]
+        result = []
+        for i1 in l1:
+            for i2 in l2:
+                if i1 == i2:
+                    result.append(i1)
+        return result
+
+    l1, l2 = [12, 4, 34, 23], [5, 12, 23, 34]
     with patch.object(CE, 'commonElements', side_effect=ce_patched):
-        assert ce.commonElements([12, 4, 34, 23], [5, 12, 23, 34]) == [12, 34, 23]
+        assert ce.commonElements(l1, l2) == [12, 34, 23]
 
 
 def test_ce_with_mock_returnValue():
@@ -27,7 +34,14 @@ def test_ce_with_mock_returnValue():
 
 def test_ce_with_mock_sideEffect():
     def ce_mocked(l1, l2):
-        return [12, 34, 23]
+        result = []
+        for i1 in l1:
+            for i2 in l2:
+                if i1 == i2:
+                    result.append(i1)
+        return result
+
+    l1, l2 = [12, 4, 34, 23], [5, 12, 23, 34]
     ce.commonElements = Mock(side_effect=ce_mocked)
-    assert ce.commonElements([12, 4, 34, 23], [5, 12, 23, 34]) == [12, 34, 23]
+    assert ce.commonElements(l1, l2) == [12, 34, 23]
 
